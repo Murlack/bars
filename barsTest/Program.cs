535037a61e57081465e31d467e753de68c43cac6
timur <timur.zagid@bk.ru>
2022-03-30 01:34:49 +0300

    Считывание символа из консоли

---
+++ barsTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace barsTest
+{
+    public delegate char EventHander(char Char);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckingEnteredCharacter Char = new CheckingEnteredCharacter();
+            Char.Run();
+        }
+    }
+    class CheckingEnteredCharacter
+    {
+        public event EventHander OnKeyPressed;
+        private char Char;
+        private char[] Char_cRreE= { 'C', 'c', 'с', 'С'};
+        public char Run()
+        {
+            while (true)
+            {
+                Char = Console.ReadKey().KeyChar;
+                for (int i = 0; i < Char_cRreE.Length; i++)
+                {
+                    if (Char == Char_cRreE[i])
+                    {
+                        Console.Clear();
+                        OnKeyPressed += WriteLineConsole;
+                        return OnKeyPressed.Invoke(Char);
+                        
+                    }
+                }
+            }
+        }
+        public char WriteLineConsole(char Char)
+        {
+            Console.WriteLine(Char);
+            return Char;
+        }
+    }
+
+    
+}
